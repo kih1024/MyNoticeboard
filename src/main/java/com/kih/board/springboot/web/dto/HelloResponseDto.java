0b90b346d08cf5f52394277e6a4ca3ae0c0d8113
kih1024 <rladlsgh654@naver.com>
2020-11-07 20:31:00 +0900

    Add JPA with H2 database

--- src/main/java/com/kih/board/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/kih/board/springboot/web/dto/HelloResponseDto.java
@@ -1,10 +1,12 @@
 package com.kih.board.springboot.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+//@AllArgsConstructor
 public class HelloResponseDto {
 
     private final String name;
